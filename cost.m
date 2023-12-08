50348a79a97337da5ed9c71535a982c9c817d1c0
Denisia Marcu <d95denis@gmail.com>
2023-12-08 13:52:11 +0100

    añadido variables de rumbo a trim y cost

--- cost.m
+++ cost.m
@@ -1,8 +1,8 @@
-function [out] = cost(s, geom, I, xcg, h, vt, gamma, TR)
+function [out] = cost(s, geom, I, xcg, h, vt, gamma, TR, xe, ye)
     
     %TR=Turn Rate
     
-    [x,u] = s_to_x_u(s, h, vt, gamma, TR);
+    [x,u] = s_to_x_u(s, h, vt, gamma, TR, xe, ye);
     
     xd = xdf(x, u, geom, I, xcg);
     out = [xd(1) xd(2) xd(5) xd(6) xd(3) xd(7)];
--- trim.m
+++ trim.m
@@ -4,16 +4,15 @@ vt    = 502*0.3048;
 h     = 0;
 gamma = 0;
 TR    = 0;
-PU    = 0.1;
 xcg   = -0.3;
 
 [geom, I] = F16();
 s0 = [0.3 4.1e-5 -7 -6.2e-4 0.01655 1];
 
-f = @(y) cost(y, geom, I, xcg, h, vt, gamma, TR);
+f = @(y) cost(y, geom, I, xcg, h, vt, gamma, TR, xe, ye);
 [strim,fval]=fsolve(f,s0);
 
-[xtrim, utrim]  = s_to_x_u(strim, h, vt, gamma, TR);
+[xtrim, utrim]  = s_to_x_u(strim, h, vt, gamma, TR, xe, ye);
 
  xd_trim = xdf(xtrim, utrim, geom, I, xcg);
  
