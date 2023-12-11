1a3b2d9a60d026f9eb3fc2c01f8ccf99e4af6d2a
Denisia Marcu <d95denis@gmail.com>
2023-12-11 18:30:10 +0100

    Cambiado condiciones de referencia

--- test_modos_propios.m
+++ test_modos_propios.m
@@ -2,17 +2,17 @@ clear
 clc
 
 % Condición de vuelo para el análisis dinámico
-vt    = 180;
-h     = 10000*0.3048;
+vt    = 140;
+h     = 1000;
 gamma = 0*pi/180;
 TR    = 0;
-xcg   = 0;
+xcg   = 0.3;
 psi   = 0;
 
 [geom, I] = F16();
 
 [xtrim, utrim] = trim(vt, h(end), gamma, TR, psi, xcg, geom, I);
-A = jacob(xtrim, utrim, geom, I, xcg);
+[A, B] = jacob(xtrim, utrim, geom, I, xcg);
     
 [phug,  phug_m, short,  short_m] = modos_propios_long(A);
 [dutch, ~, roll_tau, ~, spiral_tau, ~] = modos_propios_lat_dir(A);
