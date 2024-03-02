9374d5bcf7965c1150f2382dc299f00df5bee924
Denisia Marcu <d95denis@gmail.com>
2024-03-02 12:49:47 +0100

    comentarios añadidos para el bucle cerrado

--- lqr_ld.m
+++ lqr_ld.m
@@ -1,30 +1,38 @@
-espacio_estado_cr
+espacio_estado_cr       % Se cargan los smas LTI del movimiento del avion
+
+% Se obtiene el sma actuado correspondiente al mov. lateral-direccional
 [sys_lda, Alda, Blda, Clda, Dlda] = actuador(sys_ld,1/20.2,2);
 
 
 % Definicion de la estructura de Q y de R
-Q=diag([5 1 10 1 zeros(1,length(Alda)-4)]);
+Q=diag([5 1 10 1 zeros(1,2)]);
 R=[1 0;0 2];
-rank(obsv(sqrt(Q),Alda))
+
+% Definicion de la condicion inicial
+x0 = [30*pi/180 zeros(1,5)];
+
+rank(obsv(sqrt(Q),Alda)) % Comprobacion de la condicion de observabilidad
 
 % Obtencion de la respuesta del sistema controlado para diferentes q
 q=[0 100 200 300 400 500];
 t = linspace(0, 6, 6001);
-u = zeros(length(t),2);
-x0 = [30*pi/180 zeros(1,length(Alda)-1)];
-y=zeros(length(t),length(Alda),length(q));
-y_controles=zeros(length(t),2,length(q));
-K = zeros(2,length(Alda),length(q));
-for i=1:length(q)
+u = zeros(length(t),2);       % Entrada del piloto para calcular respuesta
+n = length(Alda);             % Numero de variables de estado
+nq = length(q);               % Numero de valores de q que se comprueban
+y=zeros(length(t),n,nq);      % Respuesta temporal del sistema
+u_c=zeros(length(t),2,nq);    % Correccion comandada por el actuador
+K = zeros(2,n,nq);            % Matrices de ganancias para cada q probada
+
+for i=1:nq
     if q(i)==0
-        K(:,:,i) = zeros(2,length(Alda));
+        K(:,:,i) = zeros(2,n);
     else
         K(:,:,i) = lqr(sys_lda,q(i)*Q,R);
     end
     A_lqr = Alda-Blda*K(:,:,i);
     controlado = ss(A_lqr,Blda,Clda,Dlda);
     y(:,:,i) = lsim(controlado,u,t,x0);
-    y_controles(:,:,i) = -lsim(ss(0,zeros(1,length(Alda)),[0; 0],K(:,:,i)),y(:,:,i),t,0);
+    u_c(:,:,i) = -lsim(ss(0,zeros(1,n),[0; 0],K(:,:,i)),y(:,:,i),t,0);
 end
 
 
--- plot_lqr_ld.m
+++ plot_lqr_ld.m
@@ -1,4 +1,4 @@
-lqr_ld
+lqr_ld      % Se cargan las respuestas del sma para diferentes q
 FontSizeTitle=12;
 FontSizeAxis=10;
 LineWidth=2;
@@ -6,7 +6,7 @@ LineWidth=2;
 variables = {' \beta', ' p', ' r', ' \phi'};
 unidad = {'rad','rad/s','rad/s','rad'};
 figure(1);
-for j=1:4
+for j=1:4       % j recorre las variables de estado a pintar
     subplot(2, 2, j)
     title(strcat('Respuesta de',variables{j},' controlada para diferentes q'),'Fontsize',FontSizeTitle);
     hold on
@@ -29,31 +29,17 @@ saveas(gcf,'x_lqr_ld.jpg');
 entradas = {" u_a", " \delta_a", " u_r", " \delta_r"};
 figure(2);
 
-for j=1:2
-    subplot(2, 2, 2*j-1)
-    title(strcat(entradas{2*j-1},' en la respuesta para diferentes q'),'Fontsize',FontSizeTitle);
+for j=1:4
+    subplot(2, 2, j)    % Numero del grafico activo
+    title(strcat(entradas{j},' en la respuesta para diferentes q'),'Fontsize',FontSizeTitle);
     hold on
     
     for i= 1:length(q)
-        imp=plot(t,y_controles(:,j,i),'DisplayName',strcat('q=',num2str(q(i))),'linewidth',LineWidth); 
-    end
-    
-    legend()
-    set(gcf, 'Position',  [100, 100, 1000, 800]);
-    a=gca;
-    a.YAxis.Color = [0 0 0];
-    a.XAxis.Color = [0 0 0];
-    a.ZAxis.Color = [0 0 0];
-    ylabel('Amplitud [grados]')
-    xlabel('Tiempo [s]')
-    grid on
-
-    subplot(2, 2, 2*j)
-    title(strcat(entradas{2*j},' en la respuesta para diferentes q'),'Fontsize',FontSizeTitle);
-    hold on
-    
-    for i= 1:length(q)
-        imp=plot(t,y(:,j+4,i),'DisplayName',strcat('q=',num2str(q(i))),'linewidth',LineWidth); 
+        if mod(j,2) == 1
+            plot(t,u_c(:,(j+1)/2,i),'DisplayName',strcat('q=',num2str(q(i))),'linewidth',LineWidth); 
+        else
+            plot(t,y(:,4+j/2,i),'DisplayName',strcat('q=',num2str(q(i))),'linewidth',LineWidth); 
+        end
     end
     
     legend()
