e64698898e05619f9a542a6d6d78cc851711e12a
Denisia Marcu <d95denis@gmail.com>
2024-03-02 11:35:30 +0100

    funcion actuador comentarios incluidos

--- actuador.m
+++ actuador.m
@@ -1,10 +1,19 @@
-function [actuado, Aa, Ba,Ca,Da] = actuador(avion,tau_a,n)      %n numero de entradas
-    [A,B]=ssdata(avion);
-    m = size(A, 1)+n; %numero de variables de estado
+function [actuado, Aa, Ba,Ca,Da] = actuador(avion,tau_a,n)      % n numero de entradas
+    % Inputs
+        % avion: sma LTI que modela el movimiento del avion
+        % tau_a: constante de tiempo del actuador
+        % n: numero de entradas al sistema que se desea usar (1 o 2)
+    % Outputs
+        % actuado: sma LTI que incluye el efecto de los actuador(es) en las
+        % entradas
+        % Aa, Ba, Ca, Da: matrices de "actuado"
+
+    [A,B]=ssdata(avion);        % Obtencion de las matrices A, B a partir del sma 
+    m = size(A, 1)+n;           % Numero de variables de estado
     Aa=[A B(:,1:n);zeros(n, 4) -1/tau_a*eye(n)];
     Ba=[zeros(4, n); 1/tau_a*eye(n)];
     Ca=eye(m);
     Da=zeros(m, n);
-    actuado = ss(Aa,Ba,Ca,Da);
+    actuado = ss(Aa,Ba,Ca,Da); 
 
 end
